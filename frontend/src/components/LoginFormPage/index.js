764e3d7b39ac16ba54fe951e39c900062948379e
joshua <joshuagmiller2289@gmail.com>
2021-02-19 09:07:43 -0800

    loginpage setup

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { login } from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import "./LoginForm.css";
+
+const LoginFormPage = () => {
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+
+  if (sessionUser) {
+    return <Redirect to="/" />;
+  }
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(login({ credential, password })).catch(async (res) => {
+      const data = await res.json();
+      if (data && data.errors) setErrors(data.errors);
+    });
+  };
+  return (
+    <div>
+      <form onSubmit={submitLogin}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <label>
+          Username or Email
+          <input
+            type="text"
+            placeholder="username/email"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginFormPage;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,50 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = "session/SET_USER";
+const REMOVE_USER = "session/REMOVE_USER";
+
+const setUser = (user) => ({
+  type: SET_USER,
+  user,
+});
+
+const deleteSession = () => ({
+  type: REMOVE_USER,
+});
+
+export const login = ({ credential, password }) => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({ credential: credential, password: password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) throw response;
+  const data = await response.json();
+  console.log(data);
+  dispatch(setUser(data.user));
+  return data;
+};
+
+const initialState = {
+  user: null,
+};
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.user;
+      return newState;
+    case REMOVE_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
