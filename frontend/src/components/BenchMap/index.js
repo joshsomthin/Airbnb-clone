3a5dcd64b3f4defd6425e5e8bb87aa15d0356cb9
joshua <joshuagmiller2289@gmail.com>
2021-02-26 09:13:01 -0800

    structure for markers working

--- frontend/src/components/BenchMap/index.js
+++ frontend/src/components/BenchMap/index.js
@@ -1,6 +1,13 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 
 const MapContainer = () => {
+  const [selected, setSelected] = useState({});
   const key = process.env.REACT_APP_GOOGLE_MAPS_API;
   const mapStyles = {
     height: "80vh",
@@ -11,13 +18,34 @@ const MapContainer = () => {
     lat: 47.6062,
     lng: -122.3321,
   };
+
+  const onSelect = (item) => {
+    setSelected(item);
+  };
   return (
     <LoadScript googleMapsApiKey={key}>
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={14}
-        center={defaultCenter}
-      ></GoogleMap>
+      <GoogleMap mapContainerStyle={mapStyles} zoom={14} center={defaultCenter}>
+        <Marker
+          onClick={(item) =>
+            onSelect({
+              name: "My Spot",
+              location: { lat: 47.606880969482496, lng: -122.3339539210074 },
+            })
+          }
+          key="Space Needle"
+          position={{ lat: 47.606880969482496, lng: -122.3339539210074 }}
+        >
+          {selected.location && (
+            <InfoWindow
+              position={selected.location}
+              clickable={true}
+              onCloseClick={() => setSelected({})}
+            >
+              <p>{selected.name}</p>
+            </InfoWindow>
+          )}
+        </Marker>
+      </GoogleMap>
     </LoadScript>
   );
 };
--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,12 +1,22 @@
 import BenchIndex from "../BenchIndex";
-import SearchContainer from "../SearchContainer";
 import BenchMap from "../BenchMap";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { locationPopulate } from "../../store/locations";
+import "./Search.css";
 
 const Search = () => {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(locationPopulate()).then((req) => setIsLoaded(true));
+  }, [dispatch]);
   return (
-    <div>
+    <div className="box-container">
+      <div>
+        <BenchIndex />
+      </div>
       <BenchMap />
-      <BenchIndex />
     </div>
   );
 };
