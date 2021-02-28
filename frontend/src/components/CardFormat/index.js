ee3e7cad70f05d61479c45f3ccc2af5200d26e4c
joshua <joshuagmiller2289@gmail.com>
2021-02-28 14:00:01 -0800

    updates recently added spots

--- frontend/src/components/CardFormat/index.js
+++ frontend/src/components/CardFormat/index.js
@@ -1,10 +1,15 @@
 import { NavLink } from "react-router-dom";
 import "./CardFormat.css";
-const CardFormat = ({ route, image, location }) => {
+const CardFormat = ({ route, image, location, guests }) => {
   return (
     <NavLink to={route} className="places-card">
       <img src={image} />
-      <div>{location}</div>
+      <div className="outer-text-div">
+        <div className="inner-text-div">
+          <div style={{ fontWeight: "700" }}>{location}</div>
+          <div>{guests} guests</div>
+        </div>
+      </div>
     </NavLink>
   );
 };
